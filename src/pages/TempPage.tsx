78e097004fafbcbf7112d4f45f14077dc3d4845b
kjefferson <kjefferson@mitre.org>
2023-03-18 17:05:48 -0400

    Trying without mongoose- get collection

--- src/pages/TempPage.tsx
+++ src/pages/TempPage.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react'
-
+import React from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,42 +9,8 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+
 export default function TempPage() {
-
-  interface State {
-    _id: string
-    name: string
-    population: string
-    code: string
-  }
-
-  /**
-   * State values
-   */
-  //const [dataFromBackend, setDataFromBackend] = React.useState<string>("");
-  const [dataFromBackend, setDataFromBackend] = React.useState<Array<State>>([]);
-
-
-  // Backend url (dev)
-  const backendUrl = 'http://localhost:3001';
-  // Backend url (production virtual machine)
-  // const backendUrl = '/api';
-
-  React.useEffect(() => {
-    // Fetch jurisdictions
-    //const getJurisdictionsUrl = `${backendUrl}/getCollection`
-    console.log('before')
-    const getJurisdictionsUrl = '/api/getStates'
-    console.log('after')
-    //const getJurisdictionsUrl = `/api/getStates`
-    fetch(getJurisdictionsUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log('heres the data for the states', data)
-        setDataFromBackend(data);
-      })
-  }, []);
-
   ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -55,102 +20,311 @@ export default function TempPage() {
     Legend
   );
 
-  //Creating chart 
-  //state labels
-  const getlabels = () => {
 
-    var dataArr: Array<State> = [];
+  /**
+   * State values
+   */
+   const [dataFromBackend, setDataFromBackend] = React.useState<string>("");
+   
+  const backendUrl = 'http://localhost:3001';
+
+  //state labels
+  const getlabels=()=> {
+    var dataArr = [];
+
     try {
-      dataArr = dataFromBackend;
+      dataArr = JSON.parse(dataFromBackend); 
     } catch (error) {
       console.log(error)
     }
 
-    let dataMap = new Map<string, number>();
+    var states = [];
 
     try {
-      for (var i = 1; i < dataArr.length; i++) {
-        dataMap.set(dataArr[i].name, parseInt(dataArr[i].population));
+      for (var i = 0; i < dataArr[0].stateBasedData.length; i++){
+        states.push(dataArr[0].stateBasedData[i].state);
+        
       }
-
-      dataMap = new Map([...dataMap.entries()].sort((a, b) => a[1] - b[1]));
-      var states = Array.from(dataMap.keys());
       return states;
     } catch (error) {
       console.log(error)
     }
+    
   }
-  const getPopulation = () => {
 
-    var dataArr: Array<State> = [];
+  //total life expectancies
+  const getlifeexp=(props)=> {
+    var dataArr = [];
+
     try {
-      dataArr = dataFromBackend;
+      dataArr = JSON.parse(dataFromBackend);  
     } catch (error) {
       console.log(error)
     }
 
-    let dataMap = new Map<string, number>();
+    var leList = [];
 
-    try {
-      for (var i = 1; i < dataArr.length; i++) {
-        dataMap.set(dataArr[i].name, parseInt(dataArr[i].population));
-      }
-
-      dataMap = new Map([...dataMap.entries()].sort((a, b) => a[1] - b[1]));
-      var populations = Array.from(dataMap.values());
-
-      return populations;
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const states = getlabels();
-  const populations = getPopulation();
-
-  //state population data
-  var data = {
-    labels: states,
-    datasets: [
-      {
-        barThickness: 4,
-
-        label: 'Number of people',
-        data: populations,
-        borderColor: ['rgb(255, 99, 132)'],
-        backgroundColor: ['rgba(255, 99, 132)'],
-      }
-    ]
-  }
-
-  //state population options
-  var options = {
-    scales: {
-      x: {
-        ticks: {
-          autoSkip: false,
-          maxRotation: 50,
-          minRotation: 30,
+    try{
+      if (props === "Total"){
+        for (var i = 0; i < dataArr[0].stateBasedData.length; i++){
+          var row = dataArr[0].stateBasedData[i].categories[0].lifeExpectancy.sex[0].Total;
+          leList.push(row)
+        }
+      } 
+      if (props === "Female"){
+        for (var i = 0; i < dataArr[0].stateBasedData.length; i++){
+          var row = dataArr[0].stateBasedData[i].categories[0].lifeExpectancy.sex[2].Female;
+          leList.push(row)
         }
       }
-    },
-    plugins: {
-      title: {
-        display: true,
-        text: 'Population by US state',
+        if (props === "Male"){
+          for (var i = 0; i < dataArr[0].stateBasedData.length; i++){
+            var row = dataArr[0].stateBasedData[i].categories[0].lifeExpectancy.sex[1].Male;
+            leList.push(row)
+          }
+        }
+    } catch (error) {
+        console.log(error)    
       }
-    }
+    return leList;
   }
 
-  return (
-    <section>
-      <h1>Health Status Dashboard</h1>
-      <Bar
+
+  React.useEffect(() => {
+    // Fetch jurisdictions
+    //const getJurisdictionsUrl = `${backendUrl}/api/getCollection`
+    const getJurisdictionsUrl = '/api/getCollection'
+    fetch(getJurisdictionsUrl)
+      .then(response => response.json())
+      .then(data => {
+        setDataFromBackend(JSON.stringify(data));
+      })
+  }, []);
+  
+
+  //life expectancy chart data
+  var data = {
+    labels: getlabels(),
+    datasets: [
+    {
+      barThickness:4,
+     
+      label: 'Total Average Years',
+      data: getlifeexp("Total"),
+      borderColor: ['rgb(255, 99, 132)'],
+      backgroundColor: ['rgba(255, 99, 132)'],
+    },
+    {
+      barThickness:4,
+     
+      label: 'Female Average years',
+      data: getlifeexp("Female"),
+      borderColor: ['rgba(53, 162, 235)'],
+      backgroundColor: ['rgba(53, 162, 235)'],
+    },
+    {
+      barThickness:4,
+     
+      label: 'Male Average years',
+      data: getlifeexp("Male"),
+      borderColor: ['rgb(75, 192, 192)'],
+      backgroundColor: ['rgb(75, 192, 192)'],
+    },
+    ]
+    }
+    
+     var options = {
+      indexAxis: 'y' as const,
+      elements: {
+        bar: {
+          borderWidth: 3,
+        },
+      },
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'right' as const,
+        },
+        title: {
+          display: true,
+          text: 'Life Expectancy by state',
+        },
+      },
+    };
+    
+  return <div>
+    
+    <Bar 
         data={data}
-        height={500}
-        width={1200}
+        height={800}
+        width={800} 
         options={options}
-      />
-    </section>
-  );
+    />
+    </div>
 }
+
+
+
+
+
+
+
+// import * as React from 'react'
+
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+// import { Bar } from 'react-chartjs-2';
+// export default function TempPage() {
+
+//   interface State {
+//     _id: string
+//     name: string
+//     population: string
+//     code: string
+//   }
+
+//   /**
+//    * State values
+//    */
+//   //const [dataFromBackend, setDataFromBackend] = React.useState<string>("");
+//   const [dataFromBackend, setDataFromBackend] = React.useState<Array<State>>([]);
+
+
+//   // Backend url (dev)
+//   const backendUrl = 'http://localhost:3001';
+//   // Backend url (production virtual machine)
+//   // const backendUrl = '/api';
+
+//   React.useEffect(() => {
+//     // Fetch jurisdictions
+//     //const getJurisdictionsUrl = `${backendUrl}/getCollection`
+//     console.log('before')
+//     //const getJurisdictionsUrl = '/api/getStates'
+
+
+//     const getJurisdictionsUrl = 'http://localhost:3001/api/getStates'
+//     console.log('after')
+//     //const getJurisdictionsUrl = `/api/getStates`
+//     fetch(getJurisdictionsUrl)
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log('heres the data for the states', data)
+//         setDataFromBackend(data);
+//       })
+//   }, []);
+
+//   ChartJS.register(
+//     CategoryScale,
+//     LinearScale,
+//     BarElement,
+//     Title,
+//     Tooltip,
+//     Legend
+//   );
+
+//   //Creating chart 
+//   //state labels
+//   const getlabels = () => {
+
+//     var dataArr: Array<State> = [];
+//     try {
+//       dataArr = dataFromBackend;
+//     } catch (error) {
+//       console.log(error)
+//     }
+
+//     let dataMap = new Map<string, number>();
+
+//     try {
+//       for (var i = 1; i < dataArr.length; i++) {
+//         dataMap.set(dataArr[i].name, parseInt(dataArr[i].population));
+//       }
+
+//       dataMap = new Map([...dataMap.entries()].sort((a, b) => a[1] - b[1]));
+//       var states = Array.from(dataMap.keys());
+//       return states;
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+//   const getPopulation = () => {
+
+//     var dataArr: Array<State> = [];
+//     try {
+//       dataArr = dataFromBackend;
+//     } catch (error) {
+//       console.log(error)
+//     }
+
+//     let dataMap = new Map<string, number>();
+
+//     try {
+//       for (var i = 1; i < dataArr.length; i++) {
+//         dataMap.set(dataArr[i].name, parseInt(dataArr[i].population));
+//       }
+
+//       dataMap = new Map([...dataMap.entries()].sort((a, b) => a[1] - b[1]));
+//       var populations = Array.from(dataMap.values());
+
+//       return populations;
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+//   const states = getlabels();
+//   const populations = getPopulation();
+
+//   //state population data
+//   var data = {
+//     labels: states,
+//     datasets: [
+//       {
+//         barThickness: 4,
+
+//         label: 'Number of people',
+//         data: populations,
+//         borderColor: ['rgb(255, 99, 132)'],
+//         backgroundColor: ['rgba(255, 99, 132)'],
+//       }
+//     ]
+//   }
+
+//   //state population options
+//   var options = {
+//     scales: {
+//       x: {
+//         ticks: {
+//           autoSkip: false,
+//           maxRotation: 50,
+//           minRotation: 30,
+//         }
+//       }
+//     },
+//     plugins: {
+//       title: {
+//         display: true,
+//         text: 'Population by US state',
+//       }
+//     }
+//   }
+
+//   return (
+//     <section>
+//       <h1>Health Status Dashboard</h1>
+//       <Bar
+//         data={data}
+//         height={500}
+//         width={1200}
+//         options={options}
+//       />
+//     </section>
+//   );
+// }
