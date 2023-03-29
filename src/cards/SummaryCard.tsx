8227cd86a9beed7ebfa38a269275544ee1781298
rpassas <rpassas@mitre.org>
2023-03-29 13:45:09 -0400

    added data and reformatted css

--- src/cards/SummaryCard.tsx
+++ src/cards/SummaryCard.tsx
@@ -1,6 +1,6 @@
 //import React from "react";
 import '../index.css';
-import { Summary } from '../interfaces/Summary';
+import { Summary } from '../interfaces/ISummary';
 
 type SummaryData = { data: Summary };
 
