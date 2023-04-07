0c51f5d9c1f4c0073ec50760ca13e296aa05cb34
kjefferson <kjefferson@mitre.org>
2023-04-06 22:25:03 -0400

    took out temp page import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import TempPage from './pages/TempPage'
 import Home from './pages/Home'
 import Regions from './pages/Regions'
 import 'bootstrap/dist/css/bootstrap.min.css';
