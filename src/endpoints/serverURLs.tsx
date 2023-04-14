860c2250812af8d387f354f67a6f788b94ad53e2
rpassas <rpassas@mitre.org>
2023-04-14 16:56:35 -0400

    adding new endpoint to secret button

--- src/endpoints/serverURLs.tsx
+++ src/endpoints/serverURLs.tsx
@@ -1,50 +1,7 @@
 
 //use when local
-export const initLifeExpectancy = `http://localhost:3001/api/initLifeExpectancy`
-export const getLifeExpectancy = `http://localhost:3001/api/getLifeExpectancy`
-export const initAlcoholTobaccoData = `http://localhost:3001/api/initAlcoholTobaccoData`
-export const getAlcoholTobaccoData = `http://localhost:3001/api/getAlcoholTobaccoData`
-export const initDCData = `http://localhost:3001/api/initDCData`
-export const getDCData = `http://localhost:3001/api/getDCData`
-export const initNAWData = `http://localhost:3001/api/initNAWData`
-export const getNAWData = `http://localhost:3001/api/getNAWData`
-export const initUSPopulationData = `http://localhost:3001/api/initUSPop`
-export const getUSPopulationData = `http://localhost:3001/api/getUSPop`
-export const initCDSummaryData = `http://localhost:3001/api/initCDSummary`
-export const getCDSummaryData = `http://localhost:3001/api/getCDSummary`
-export const initWeightSummary = `http://localhost:3001/api/initWeight`
-export const getWeightSummary = `http://localhost:3001/api/getWeight`
-export const initUninsuredSummary = `http://localhost:3001/api/initUninsuredUSPop`
-export const getUninsuredSummary = `http://localhost:3001/api/getUninsuredUSPop`
-export const initUninsuredByEducation = `http://localhost:3001/api/initUninsuredByEducation`
-export const getUninsuredByEducation = `http://localhost:3001/api/getUninsuredByEducation`
-export const initUninsuredByAge = `http://localhost:3001/api/initUninsuredByAge`
-export const getUninsuredByAge = `http://localhost:3001/api/getUninsuredByAge`
-export const initUninsuredBySubgroup = `http://localhost:3001/api/initUninsuredBySubgroup`
-export const getUninsuredBySubgroup = `http://localhost:3001/api/getUninsuredBySubgroup`
-
+export const updateDB = `http://localhost:3001/api/update-all`
 
 
 //use in VM
-// export const initLifeExpectancy = `http://healthdashboard.mitre.org/api/initLifeExpectancy`
-// export const getLifeExpectancy = `http://healthdashboard.mitre.org/api/getLifeExpectancy`
-// export const initAlcoholTobaccoData = `http://healthdashboard.mitre.org/api/initAlcoholTobaccoData`
-// export const getAlcoholTobaccoData = `http://healthdashboard.mitre.org/api/getAlcoholTobaccoData`
-// export const initDCData = `http://healthdashboard.mitre.org/api/initDCData`
-// export const getDCData = `http://healthdashboard.mitre.org/api/getDCData`
-// export const initNAWData = `http://healthdashboard.mitre.org/api/initNAWData`
-// export const getNAWData = `http://healthdashboard.mitre.org/api/getNAWData`
-// export const initUSPopulationData = `http://healthdashboard.mitre.org/api/initUSPop`
-// export const getUSPopulationData = `http://healthdashboard.mitre.org/api/getUSPop`
-// export const initCDSummaryData = `http://healthdashboard.mitre.org/api/initCDSummary`
-// export const getCDSummaryData = `http://healthdashboard.mitre.org/api/getCDSummary`
-// export const initWeightSummary = `http://healthdashboard.mitre.org/api/initWeight`
-// export const getWeightSummary = `http://healthdashboard.mitre.org/api/getWeight`
-// export const initUninsuredSummary = `http://healthdashboard.mitre.org/api/initUninsuredUSPop`
-// export const getUninsuredSummary = `http://healthdashboard.mitre.org/api/getUninsuredUSPop`
-// export const initUninsuredByEducation = `http://healthdashboard.mitre.org/api/initUninsuredByEducation`
-// export const getUninsuredByEducation = `http://healthdashboard.mitre.org/api/getUninsuredByEducation`
-// export const initUninsuredByAge = `http://healthdashboard.mitre.org/api/initUninsuredByAge`
-// export const getUninsuredByAge = `http://healthdashboard.mitre.org/api/getUninsuredByAge`
-// export const initUninsuredBySubgroup = `http://healthdashboard.mitre.org/api/initUninsuredBySubgroup`
-// export const getUninsuredBySubgroup = `http://healthdashboard.mitre.org/api/getUninsuredBySubgroup`
+// export const updateDB = `http://healthdashboard.mitre.org/api/update-all`
--- src/pages/Secret.tsx
+++ src/pages/Secret.tsx
@@ -1,42 +1,17 @@
 import * as React from 'react'
 import Container from 'react-bootstrap/Container';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import { useEffect, useRef, useState } from "react";
 
-import {
-    initLifeExpectancy, getLifeExpectancy, initAlcoholTobaccoData, getAlcoholTobaccoData, initDCData, getDCData,
-    initNAWData, getNAWData, initUSPopulationData, getUSPopulationData, initCDSummaryData, getCDSummaryData, initWeightSummary, getWeightSummary,
-    initUninsuredSummary, getUninsuredSummary, initUninsuredByEducation, getUninsuredByEducation, initUninsuredByAge, getUninsuredByAge,
-    initUninsuredBySubgroup, getUninsuredBySubgroup, initBirthRateData, getBirthRateData, initGestBirthRates, getGestBirthRates, init12MonthBirthRates, get12MonthBirthRates
-} from '../endpoints/HomePageServerURLs'
 
-import { initRecentYearDCModel, getRecentYearDCModelAllCauses, getRecentYearDCModelAlzheimer, getRecentYearDCModelCovid19, getRecentYearDCModelCancer, getRecentYearDCModelLiver, getRecentYearDCModelRespiratory, getRecentYearDCModelDiabetes, getRecentYearDCModelHeart, getRecentYearDCModelHIV, getRecentYearDCModelHypertension, getRecentYearDCModelFluPneu, getRecentYearDCModelKidney, getRecentYearDCModelParkinsons, getRecentYearDCModelPneumonionitis, getRecentYearDCModelSepticimia, getRecentYearDCModelStroke, initRecent3YearDCModel, getRecent3YearDCModel } from '../endpoints/RegionsPageServerURLs'
-
+import { updateDB } from '../endpoints/serverURLs'
 
 //TODO replace this reset button with API call 
 async function initData(event: React.MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
     try {
-        var response = await fetch(initRecentYearDCModel);
-        response = await fetch(initRecent3YearDCModel);
-        response = await fetch(initLifeExpectancy);
-        response = await fetch(initAlcoholTobaccoData);
-        response = await fetch(initDCData); //TODO resaves
-        response = await fetch(initNAWData); //TODO resaves
-        response = await fetch(initUSPopulationData); //TODO resaves
-        response = await fetch(initCDSummaryData); //TODO resaves
-        response = await fetch(initWeightSummary); //TODO resaves
-        response = await fetch(initUninsuredSummary); //TODO resaves
-        response = await fetch(initUninsuredByEducation); //TODO resaves
-        response = await fetch(initUninsuredByAge); //TODO resaves
-        response = await fetch(initUninsuredBySubgroup); //TODO resaves
-        response = await fetch(initBirthRateData); //TODO resaves
-        response = await fetch(initGestBirthRates); //TODO resaves
-        response = await fetch(init12MonthBirthRates); //TODO resaves
+        var response = await fetch(updateDB);
         const { status } = response;
         return status;
     } catch (err) {
@@ -45,7 +20,7 @@ async function initData(event: React.MouseEvent<HTMLButtonElement>) {
     }
 }
 
-export default function Regions() {
+export default function Secret() {
 
 
     return (
