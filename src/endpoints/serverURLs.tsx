758c9b48afabea9e948e1d96891fadc5dcb8ae8c
rpassas <rpassas@mitre.org>
2023-04-14 17:06:36 -0400

    wrong url for production

--- src/endpoints/serverURLs.tsx
+++ src/endpoints/serverURLs.tsx
@@ -1,7 +1,7 @@
 
 //use when local
-export const updateDB = `http://localhost:3001/api/update-all`
+//export const updateDB = `http://localhost:3001/api/update-all`
 
 
 //use in VM
-// export const updateDB = `http://healthdashboard.mitre.org/api/update-all`
+export const updateDB = `http://healthdashboard.mitre.org/api/update-all`
